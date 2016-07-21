f198dde7ce3a0fce2e61b6d6a5a7c7b16139ab3c
danik <me@subsonic.cz>
2016-07-21 20:56:34 +0200

    added DI bridge

---
+++ src/js/Nittro/Ajax/Bridges/AjaxDI.js
@@ -0,0 +1,25 @@
+_context.invoke('Nittro.Ajax.Bridges', function(Nittro) {
+
+    if (!Nittro.DI) {
+        return;
+    }
+
+    var AjaxDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
+        AjaxDI.Super.call(containerBuilder, config);
+    }, {
+        load: function() {
+            var builder = this._getContainerBuilder();
+
+            builder.addServiceDefinition('ajax', {
+                factory: 'Nittro.Ajax.Service()',
+                run: true,
+                setup: [
+                    '::addTransport(Nittro.Ajax.Transport.Native())'
+                ]
+            });
+        }
+    });
+
+    _context.register(AjaxDI, 'AjaxDI')
+
+});
