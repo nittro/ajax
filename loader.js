e241b7254e4f7c583f43e8225db1096daf53f9ec
danik <me@subsonic.cz>
2016-07-26 22:12:11 +0200

    updated loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
 /* jahudka/minify compatible loader */
-/*! @include file="src/js/Ajax/FormData.js" */
-/*! @include file="src/js/Ajax/Request.js" */
-/*! @include file="src/js/Ajax/Response.js" */
-/*! @include file="src/js/Ajax/Service.js" */
-/*! @include file="src/js/Ajax/Transport/Native.js" */
+/*! @include file="src/js/Nittro/Ajax/Request.js" */
+/*! @include file="src/js/Nittro/Ajax/Response.js" */
+/*! @include file="src/js/Nittro/Ajax/Service.js" */
+/*! @include file="src/js/Nittro/Ajax/Transport/Native.js" */
+/*! @include file="src/js/Nittro/Ajax/Bridges/AjaxDI.js" */
 
